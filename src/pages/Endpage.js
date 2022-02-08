077d46624e094dab7531ed0274a4d974ee472f2f
timh <tim@tim.hansen.se>
2022-02-08 10:20:09 +0100

    homebutton for endpage

--- src/pages/Endpage.js
+++ src/pages/Endpage.js
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 //Components
 import AnimatedPage from '../components/AnimatedPage';
 //Style & Animation
@@ -12,31 +13,37 @@ import {
   titleAnimB,
 } from '../animations';
 
-export const AnimateHeart = () => (
-  <AnimatedPage>
-    <HeartContainer>
-      <motion.div
-        variants={pageAnimation}
-        initial="hidden"
-        animate="show"
-        exit="exit"
-      >
-        <motion.div variants={titleAnimB}>
-          <Heart animate="show" variants={heartAnimation}>
-            {' '}
-            <h1>THANK YOU!</h1>
-            <img src={heart} alt="heart" />
-          </Heart>
+const AnimateHeart = () => {
+  const navigate = useNavigate();
+  return (
+    <AnimatedPage>
+      <HeartContainer>
+        <motion.div
+          variants={pageAnimation}
+          initial="hidden"
+          animate="show"
+          exit="exit"
+        >
+          <motion.div variants={titleAnimB}>
+            <Heart animate="show" variants={heartAnimation}>
+              {' '}
+              <h1>THANK YOU!</h1>
+              <img src={heart} alt="heart" />
+            </Heart>
+          </motion.div>
+          <motion.div variants={endFade} className="legend">
+            <motion.p /* variants={endFade} */>
+              You're a legend! Thank you for saving our grades in school!
+            </motion.p>
+            <motion.div className="btnBox">
+              <motion.button onClick={() => navigate('/')}>HOME</motion.button>
+            </motion.div>
+          </motion.div>
         </motion.div>
-        <div className="legend">
-          <motion.p variants={endFade}>
-            You're a legend! Thank you for saving our grades in school!
-          </motion.p>
-        </div>
-      </motion.div>
-    </HeartContainer>
-  </AnimatedPage>
-);
+      </HeartContainer>
+    </AnimatedPage>
+  );
+};
 
 //Styles
 const HeartContainer = styled.div`
@@ -65,6 +72,59 @@ const HeartContainer = styled.div`
     @media (max-width: 1280px) {
       /* font-size: 3rem; */
     }
+  }
+  .btnBox {
+    display: flex;
+    justify-content: center;
+  }
+  button {
+    --color: #3d3d3d;
+    font-family: inherit;
+    display: inline-block;
+    width: 8em;
+    height: 2.6em;
+    line-height: 2.5em;
+    margin: 20px;
+    position: relative;
+    overflow: hidden;
+    border: 2px solid var(--color);
+    transition: color 0.5s;
+    z-index: 1;
+    font-size: 17px;
+    border-radius: 6px;
+    font-weight: 500;
+    color: var(--color);
+    background: none;
+  }
+
+  button:before {
+    content: '';
+    position: absolute;
+    z-index: -1;
+    background: var(--color);
+    height: 150px;
+    width: 200px;
+    border-radius: 50%;
+  }
+
+  button:hover {
+    color: #fff;
+  }
+
+  button:before {
+    top: 100%;
+    left: 100%;
+    transition: all 0.7s;
+  }
+
+  button:hover:before {
+    top: -30px;
+    left: -30px;
+  }
+
+  button:active:before {
+    background: #df6af8;
+    transition: background 0s;
   }
 `;
 
